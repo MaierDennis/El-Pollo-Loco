afdd23ee908d35e269cdddff518d698cca9072e6
Dennis Maier <maier.dennis@outlook.de>
2023-01-05 11:23:32 +0100

    next

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -1,9 +1,13 @@
 class Chicken extends MovableObject {
 
-
+    height = 60;
+    width = 80;
+    y = 370;
 
     constructor(){
         super().loadImage('assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 200 + Math.random() * 500; //Zahl zwischen 200 und 700
     }
 
 }
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -6,10 +6,47 @@ class World {
         new Chicken(),
         new Chicken()
     ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('assets/img/5_background/layers/air.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0)
+    ];
+    canvas;
+    ctx;
 
-
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addObjectsToMap(this.backgroundObjects)
+        this.addObjectsToMap(this.clouds)
+        this.addObjectsToMap(this.enemies)
+        this.addToMap(this.character);
+        
+
+        //draw wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects){
+        objects.forEach(o => {
+            this.addToMap(o);
+        })
+    }
+
+    addToMap(mo){
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
